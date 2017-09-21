6118d1201b764340b0665fbc37db24ef0519a6a7
Lucas Nascimento Santos Souza <lucasnascimentoss@gmail.com>
2017-09-21 15:28:28 -0300

    Algoritmo a partir do centro
    Funcionando para os Predios, porem nao para a Praca dos 3 poderes

--- prog2_l.m
+++ prog2_l.m
@@ -10,10 +10,10 @@ clear all;
 close all;
 
 %% Imagens a serem processadas
-imgs = fullfile('imagens_praca3poderes_menores');
+imgs = fullfile('imagens_predio');
 imgSet = imageSet(imgs);
-%numImages = imgSet.Count;
-numImages = 2;
+numImages = imgSet.Count;
+center = round(numImages/2);
 
 % Mostra as imagens que serao processadas
 figure(1), montage(imgSet.ImageLocation), title('Imagens originais'), pause;
@@ -21,20 +21,21 @@ figure(1), montage(imgSet.ImageLocation), title('Imagens originais'), pause;
 % Inicializando vetor de homografias estimadas
 homographies(numImages) = projective2d(eye(3));
 
-%% Primeira Imagem
+%% Imagem Central
 % Leitura da Primeira Imagem
-img = readimage(imgSet, 1);
+img = readimage(imgSet, center);
 % Detectando features e pontos para a imagem (1)
 grayImage = rgb2gray(img);
 points = detectSURFFeatures(grayImage);
 [features, points] = extractFeatures(grayImage, points);
 
 %% Imagens subsequentes em pares
-for n = 2:numImages
+%% Centro para a direita
+for n = (center-1):1:numImages
     %% Imagem (n-1)
     % Le a imagem (n-1).
     imgPrevious = readimage(imgSet, n-1);
-    % Guarda os pontos e features da imagem I(n-1).
+    % Guarda os pontos e features da imagem (n-1).
     pointsPrevious = points;
     featuresPrevious = features;
     % Selecionando os pontos mais relevantes
@@ -45,7 +46,7 @@ for n = 2:numImages
     %% Imagem (n)
     % Le a imagem (n).
     img = readimage(imgSet, n);
-    % Detectando features e pontos para a imagem I(n)
+    % Detectando features e pontos para a imagem (n)
     grayImage = rgb2gray(img);
     points = detectSURFFeatures(grayImage);
     [features, points] = extractFeatures(grayImage, points);
@@ -68,13 +69,61 @@ for n = 2:numImages
     % Mostra os inliers obtidos a partir da homografia aproximada
     figure(5), showMatchedFeatures(imgPrevious, img, inlierPtsPrev, inlierPts),title( ['Pontos de interesse em comum entre a Imagem ' num2str(n-1) '(R) e a Imagem ' num2str(n) '(GB), sem outliers' ] ), legend(['Pontos de interesse da Imagem ' num2str(n-1) ], ['Pontos de interesse da Imagem ' num2str(n) ]),  pause;
     
+    %% Acumulacao das transformadas
+    homographies(n).T = homographies(n-1).T * homographies(n).T;
+    disp(homographies(n).T);
 end
+
+%% Centro para a esquerda
+for m = (center+1):-1:1
+    %% Imagem (m+1)
+    % Le a imagem (m+1).
+    imgPrevious = readimage(imgSet, m+1);
+    % Guarda os pontos e features da imagem (m+1).
+    pointsPrevious = points;
+    featuresPrevious = features;
+    % Selecionando os pontos mais relevantes
+    strongestPointsPrev = pointsPrevious.selectStrongest(100);
+    % Mostrando os pontos de interesse da imagem
+    figure(2), imshow(imgPrevious), hold on, title( ['Pontos de interesse da Imagem ' num2str(m+1) ] ), strongestPointsPrev.plot('showOrientation',true), pause;
+
+    %% Imagem (n)
+    % Le a imagem (n).
+    img = readimage(imgSet, m);
+    % Detectando features e pontos para a imagem (m)
+    grayImage = rgb2gray(img);
+    points = detectSURFFeatures(grayImage);
+    [features, points] = extractFeatures(grayImage, points);
+    % Selecionando os pontos mais relevantes    
+    strongestPoints = points.selectStrongest(100);
+    % Mostrando os pontos de interesse da imagem
+    figure(3), imshow(img), hold on, title( ['Pontos de interesse da Imagem ' num2str(m) ] ), strongestPoints.plot('showOrientation',true), pause;
+    
+    %% Casamento entre o par de imagens (m) e (m+1).
+    indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
+    matchedPoints = points(indexPairs(:,1), :);
+    matchedPointsPrev = pointsPrevious(indexPairs(:,2), :);
+    % Mostra os pontos de interesse em comum entre o par de imagens atual
+    figure(4), showMatchedFeatures(imgPrevious, img, matchedPointsPrev, matchedPoints),title( ['Pontos de interesse em comum entre a Imagem ' num2str(m+1) '(R) e a Imagem ' num2str(m) '(GB), com outliers' ] ), legend(['Pontos de interesse da Imagem ' num2str(m+1) ], ['Pontos de interesse da Imagem ' num2str(m) ]),  pause;
+    
+    %% Homografia estimada para as imagens (m) e (m+1)
+    [homographies(m),inlierPts,inlierPtsPrev] = ...
+        estimateGeometricTransform(matchedPoints,matchedPointsPrev,...
+        'projective', 'Confidence', 99.9, 'MaxNumTrials', 2000);
+    % Mostra os inliers obtidos a partir da homografia aproximada
+    figure(5), showMatchedFeatures(imgPrevious, img, inlierPtsPrev, inlierPts),title( ['Pontos de interesse em comum entre a Imagem ' num2str(m+1) '(R) e a Imagem ' num2str(m) '(GB), sem outliers' ] ), legend(['Pontos de interesse da Imagem ' num2str(m+1) ], ['Pontos de interesse da Imagem ' num2str(m) ]),  pause;
+    
+    %% Acumulacao das transformadas
+    homographies(m).T = homographies(m+1).T * homographies(m).T;
+    disp(homographies(m).T);
+end
+
+%% Inicializacao do Panorama
 
 % Tamanho da imagem (igual para todas) e numero total de homografias
 imageSize = size(img); 
 numHomographies = numel(homographies);
 
-%% Inicializacao do Panorama
 for i = 1:numHomographies
     [xlim(i,:), ylim(i,:)] = outputLimits(homographies(i), [1 imageSize(2)], [1 imageSize(1)]);
 end

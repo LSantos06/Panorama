8accc21a1493e00143c7f5a08bc62de69e0ae290
Filipe Teixeira Cardoso <filipetc.bsb@gmail.com>
2017-09-25 18:47:18 -0300

    + Atualizando código de referência para o orginal da documentação do matlab2014b

--- ref.m
+++ ref.m
@@ -1,59 +1,59 @@
 
 % Load images.
-imgs = fullfile('imagens_praca3poderes_menores');
-imgSet = imageSet(imgs);
+buildingDir = fullfile(toolboxdir('vision'), 'visiondemos', 'building');
+buildingScene = imageSet(buildingDir);
 
 % Display images to be stitched
-montage(imgSet.ImageLocation)
+montage(buildingScene.ImageLocation)
 
 % Read the first image from the image set.
-img = read(imgSet, 1);
+I = read(buildingScene, 1);
 
 % Initialize features for I(1)
-grayImg = rgb2gray(img);
-pts = detectSURFFeatures(grayImg);
-[fts, pts] = extractFeatures(grayImg, pts);
+grayImage = rgb2gray(I);
+points = detectSURFFeatures(grayImage);
+[features, points] = extractFeatures(grayImage, points);
 
 % Initialize all the transforms to the identity matrix. Note that the
 % projective transform is used here because the building images are fairly
 % close to the camera. Had the scene been captured from a further distance,
 % an affine transform would suffice.
-tforms(imgSet.Count) = projective2d(eye(3));
+tforms(buildingScene.Count) = projective2d(eye(3));
 
 % Iterate over remaining image pairs
-for n = 2:imgSet.Count
+for n = 2:buildingScene.Count
 
     % Store points and features for I(n-1).
-    ptsAnt = pts;
-    ftsAnt = fts;
+    pointsPrevious = points;
+    featuresPrevious = features;
 
     % Read I(n).
-    img = read(imgSet, n);
+    I = read(buildingScene, n);
 
     % Detect and extract SURF features for I(n).
-    grayImg = rgb2gray(img);
-    pts = detectSURFFeatures(grayImg);
-    [fts, pts] = extractFeatures(grayImg, pts);
+    grayImage = rgb2gray(I);
+    points = detectSURFFeatures(grayImage);
+    [features, points] = extractFeatures(grayImage, points);
 
     % Find correspondences between I(n) and I(n-1).
-    pares = matchFeatures(fts, ftsAnt, 'Unique', true);
+    indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
 
-    mtchdPts = pts(pares(:,1), :);
-    mtchdPtsAnt = ptsAnt(pares(:,2), :);
+    matchedPoints = points(indexPairs(:,1), :);
+    matchedPointsPrev = pointsPrevious(indexPairs(:,2), :);
 
     % Estimate the transformation between I(n) and I(n-1).
-    tforms(n) = estimateGeometricTransform(mtchdPts, mtchdPtsAnt,...
-    'projective', 'Confidence', 99.9, 'MaxNumTrials', 2000);
+    tforms(n) = estimateGeometricTransform(matchedPoints, matchedPointsPrev,...
+        'projective', 'Confidence', 99.9, 'MaxNumTrials', 2000);
 
     % Compute T(1) * ... * T(n-1) * T(n)
     tforms(n).T = tforms(n-1).T * tforms(n).T;
 end
 
-imgSize = size(img);  % all the images are the same size
+imageSize = size(I);  % all the images are the same size
 
 % Compute the output limits  for each transform
 for i = 1:numel(tforms)
-    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imgSize(2)], [1 imgSize(1)]);
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]);
 end
 
 avgXLim = mean(xlim, 2);
@@ -71,22 +71,22 @@ for i = 1:numel(tforms)
 end
 
 for i = 1:numel(tforms)
-    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imgSize(2)], [1 imgSize(1)]);
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]);
 end
 
 % Find the minimum and maximum output limits
 xMin = min([1; xlim(:)]);
-xMax = max([imgSize(2); xlim(:)]);
+xMax = max([imageSize(2); xlim(:)]);
 
 yMin = min([1; ylim(:)]);
-yMax = max([imgSize(1); ylim(:)]);
+yMax = max([imageSize(1); ylim(:)]);
 
 % Width and height of panorama.
 width  = round(xMax - xMin);
 height = round(yMax - yMin);
 
 % Initialize the "empty" panorama.
-panorama = zeros([height width 3], 'like', img);
+panorama = zeros([height width 3], 'like', I);
 
 blender = vision.AlphaBlender('Operation', 'Binary mask', ...
     'MaskSource', 'Input port');
@@ -97,15 +97,15 @@ yLimits = [yMin yMax];
 panoramaView = imref2d([height width], xLimits, yLimits);
 
 % Create the panorama.
-for i = 1:imgSet.Count
+for i = 1:buildingScene.Count
 
-    img = read(imgSet, i);
+    I = read(buildingScene, i);
 
     % Transform I into the panorama.
-    warpedImage = imwarp(img, tforms(i), 'OutputView', panoramaView);
+    warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
 
     % Create an mask for the overlay operation.
-    warpedMask = imwarp(ones(size(img(:,:,1))), tforms(i), 'OutputView', panoramaView);
+    warpedMask = imwarp(ones(size(I(:,:,1))), tforms(i), 'OutputView', panoramaView);
 
     % Clean up edge artifacts in the mask and convert to a binary image.
     warpedMask = warpedMask >= 1;
